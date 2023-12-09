b5ac5ab2495f7c885323bc46f298a767d24ec31b
AmazingAkai <azharmomin420@gmail.com>
2023-12-09 17:06:42 +0530

    fixed some more stuff

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,10 +8,10 @@ const Footer = () => {
   return (
     <>
       <hr className="w-full" />
-      <footer className="py-6 px-4 md:px-36 flex flex-col-reverse md:flex-row items-center justify-between gap-10 md:gap-0 md:h-[20rem]">
-        <div className="flex flex-col items-center md:items-start justify-between h-full mb-6 md:mb-0">
+      <footer className="py-6 px-4 md:px-36 flex flex-col-reverse md:flex-row items-center justify-between gap-10 md:gap-0 md:h-64">
+        <div className="flex flex-col items-center justify-between h-full mb-6 md:mb-0">
           <Image src={Logo} alt="Giftify Logo" className="w-auto h-10" />
-          <div className="flex items-center justify-center md:justify-start gap-1 text-zinc-400 text-sm">
+          <div className="flex items-center justify-center gap-1 text-zinc-400 text-sm">
             <div>&copy; 2023 Giftify, Inc.</div>
             <div className="flex items-center">
               <Link href="/github" target="_blank">
@@ -51,7 +51,7 @@ const Footer = () => {
           <div className="flex flex-col mb-4 md:mb-0">
             <h3 className="text-lg font-semibold text-center">About</h3>
             <Button asChild variant="link">
-              <Link href="/about" target="_blank" className="text-zinc-600 font-normal ">
+              <Link href="/about" className="text-zinc-600 font-normal ">
                 About
               </Link>
             </Button>
@@ -69,12 +69,12 @@ const Footer = () => {
           <div className="flex flex-col">
             <h3 className="text-lg font-semibold text-center">Legal</h3>
             <Button asChild variant="link">
-              <Link href="/privacy-policy" target="_blank" className="text-zinc-600 font-normal ">
+              <Link href="/privacy-policy" className="text-zinc-600 font-normal ">
                 Privacy Policy
               </Link>
             </Button>
             <Button asChild variant="link">
-              <Link href="/terms-of-service" target="_blank" className="text-zinc-600 font-normal ">
+              <Link href="/terms-of-service" className="text-zinc-600 font-normal ">
                 Terms Of Service
               </Link>
             </Button>
